47a91fcd5f1f2858046b4e11c70fb067a642c633
Prawit Chaivong <pchaivong@gmail.com>
2015-09-04 00:21:30 +0700

    Ignore sqlite file and add api, rest_framework app to main setting

--- wippie/wippie/settings.py
+++ wippie/wippie/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -93,7 +95,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
