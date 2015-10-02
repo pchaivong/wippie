ec1422c71ce04d2c2bb2c918f1f64bcd63c86360
pchaivong <pchaivong@gmail.com>
2015-10-02 12:45:28 +0700

    Create Holiday and UserProfile model

--- wippie/api/models.py
+++ wippie/api/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# User profiles
+# Extend django based User model
+class UserProfile(User):
+    user = models.OneToOneField(User)
+    mobile = models.CharField(max_length=16, null=True)
+    avatar = models.URLField()
+    birth = models.DateField()
+
+
+# Holiday
+# Contain holiday
+class Holiday(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=56, null=True)
+
+    def __unicode__(self):
+        return "[%s] %s" % (self.date.strftime("%Y-%m-%d"), self.description)
+
+
+
