2f9eb4aaf1b847cba1f8d3bb5b4a25ef0253a5da
Prawit Chaivong <pchaivong@gmail.com>
2015-10-03 16:18:28 +0700

    Add TSUser and use it as a auth model

--- wippie/api/admin.py
+++ wippie/api/admin.py
@@ -1,7 +1,82 @@
+from django import forms
 from django.contrib import admin
-from .models import UserProfile
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
 from .models import Holiday
+from .models import TSUser
+
+
+class TSUserCreationForm(forms.ModelForm):
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirmed Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = TSUser
+        fields = ('email', 'date_of_birth')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Password don't match")
+
+        return password2
+
+    def save(self, commit=True):
+        user = super(TSUserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class TSUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = TSUser
+        fields = ('email', 'password', 'date_of_birth', 'is_active',
+                  'is_admin', 'avatar', 'first_name', 'last_name', 'nickname', 'mobile')
+
+    def clean_password(self):
+        return self.initial["password"]
+
+class TSUserAdmin(UserAdmin):
+
+    form = TSUserChangeForm
+    add_form = TSUserCreationForm
+
+    list_display = ('email', 'date_of_birth', 'is_admin', 'first_name', 'last_name', 'nickname',
+                    'mobile', 'avatar')
+
+    list_filter = ('is_admin',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'nickname', 'date_of_birth', 'mobile', 'avatar')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'date_of_birth', 'password1', 'password2')
+        }),
+    )
+
+    search_fields = ('email',)
+    ordering = ('date_of_birth',)
+    filter_horizontal = ()
+
+
+# Register new User model
+admin.site.register(TSUser, TSUserAdmin)
+admin.site.unregister(Group)
 
 # Register your models here.
-admin.site.register(UserProfile)
 admin.site.register(Holiday)
--- wippie/api/models.py
+++ wippie/api/models.py
@@ -1,14 +1,78 @@
-from django.contrib.auth.models import User
 from django.db import models
 
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
-# User profiles
-# Extend django based User model
-class UserProfile(User):
-    user = models.OneToOneField(User)
-    mobile = models.CharField(max_length=16, null=True)
-    avatar = models.URLField()
-    birth = models.DateField()
+
+class TSUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, date_of_birth, password):
+        user = self.create_user(email,
+                                password=password,
+                                date_of_birth=date_of_birth)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class TSUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    # Personal info
+    first_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    avatar = models.ImageField(upload_to='profiles/', null=True)
+    mobile = models.CharField(max_length=32, null=True)
+    nickname = models.CharField(max_length=16, null=True)
+
+    objects = TSUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def get_full_name(self):
+        if self.first_name and self.last_name:
+            return "%s %s" % (self.first_name, self.last_name)
+        return self.email
+
+    def get_short_name(self):
+        if self.nickname:
+            return self.nickname
+        return self.get_full_name()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 
 
 # Holiday
