c1622e5beefac2216f261fa112a24b40e6fbbd3d
pchaivong <pchaivong@gmail.com>
2015-10-08 19:33:20 +0700

    Add Comments and example

--- wippie/api/urls.py
+++ wippie/api/urls.py
@@ -7,5 +7,10 @@ from .viewsets import HolidayViewSet
 
 router = routers.SimpleRouter()
 router.register(r'holiday', HolidayViewSet)
+# add your route here.
+# router.register(r'late', LateViewSet)
 
+
+
+# don't touch it
 urlpatterns = router.urls
