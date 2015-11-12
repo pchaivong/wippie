a85d712ac69c0b55478714f346644d66715284c4
Prawit Chaivong <pchaivong@gmail.com>
2015-11-12 14:54:36 +0700

    Remove field password and last_login from user model

--- wippie/api/serializers.py
+++ wippie/api/serializers.py
@@ -7,6 +7,7 @@ from .models import Holiday
 class TSUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = TSUser
+        exclude = ('password', 'last_login',)
 
 
 class HolidaySerializer(serializers.ModelSerializer):
