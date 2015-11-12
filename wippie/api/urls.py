feb219f90964eecafc60b50f00f900658e7ee1d7
Prawit Chaivong <prawit.chaivong1@thomsonreuters.com>
2015-11-12 14:56:14 +0700

    Add route to user api

--- wippie/api/urls.py
+++ wippie/api/urls.py
@@ -4,8 +4,10 @@ from rest_framework import routers
 
 # Viewsets
 from .viewsets import HolidayViewSet
+from .viewsets import TSUserViewSet
 
 router = routers.SimpleRouter()
 router.register(r'holiday', HolidayViewSet)
+router.register(r'user', TSUserViewSet)
 
 urlpatterns = router.urls
